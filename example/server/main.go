83f43697a1477e58652964a433333c383f6a704c
Julio Torres <jtorres990@gmail.com>
2025-03-16 18:06:11 -0500

    feat: migrate load testing and server implementation to separate files

---
+++ example/server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/onnasoft/ZenithSQL/network"
+	"github.com/onnasoft/ZenithSQL/response"
+	"github.com/onnasoft/ZenithSQL/server"
+	"github.com/onnasoft/ZenithSQL/statement"
+	"github.com/onnasoft/ZenithSQL/transport"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	NUM_CLIENTS                = 200
+	MAX_CONNECTIONS_PER_CLIENT = 200
+	MIN_CONNECTIONS_PER_CLIENT = 100
+	MESSAGES_PER_CLIENT        = 10000
+	TIMEOUT                    = 3 * time.Second
+	SERVER_ADDR                = "127.0.0.1:8081"
+	TOKEN                      = "my-secure-token"
+)
+
+func main() {
+	logger := logrus.New()
+
+	svr := server.NewMessageServer(&server.ServerConfig{
+		Address: ":8081",
+		Logger:  logger,
+		Timeout: 3 * time.Second,
+		Handler: func(conn *network.ZenithConnection, message *transport.Message) {
+			response, _ := response.DeserializeResponse(message.Header.MessageType, message.Body)
+			msg, _ := transport.NewResponseMessage(message, statement.NewEmptyStatement(response.Protocol()))
+			msg.Header.MessageID = message.Header.MessageID
+			msg.Header.MessageType = message.Header.MessageType
+
+			_, err := conn.Write(msg.ToBytes())
+			if err != nil {
+				logger.Info("Failed to send response:", err)
+			}
+		},
+		OnConnection: func(conn *network.ZenithConnection, stmt *statement.LoginStatement) {
+			logger.Info("New connection from ", conn.RemoteAddr(), stmt.Tags)
+		},
+		LoginValidator: func(stmt *statement.LoginStatement) bool {
+			return stmt.ValidateHash(TOKEN)
+		},
+	})
+
+	go func() {
+		err := svr.Start()
+		if err != nil {
+			log.Fatal("Failed to start server:", err)
+		}
+	}()
+
+	logger.Info("Server started at ", SERVER_ADDR)
+
+	<-make(chan struct{})
+}
